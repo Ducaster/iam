f415048e97d17622b97a59a05c1d111bc40c14c7
sy2681 <yn3819@naver.com>
2024-05-19 21:43:34 +0900

    DEV-1: history 레이아웃 변경

--- src/components/Ballon.jsx
+++ src/components/Ballon.jsx
@@ -1,20 +1,15 @@
 import "assets/base.css";
 
-export const Ballon = ({children,text,title,button}) => {
+export const Ballon = ({ children, keyword, text, title, button }) => {
   return (
-    <div className='ballon_wrap'>
-      <div className="content_wrap">
-        {children}
-      </div>
-      <div className="title_wrap">
-        {title}
-      </div>
-      <div className='ballon'>
-        {text}
-      </div>
-      <div className="btn_wrap">
-        {button}
-      </div>
+    <div className="ballon_wrap bg-gray-100 py-4 px-32 cursor-pointer hover:bg-gray-200">
+      <div className="w-36 shadow-md shadow-gray-400">{children}</div>
+      {/* <div className="keyword_wrap">
+          <span># {keyword}</span>
+        </div>
+        <div className="title_wrap">{title}</div>
+        <div className="ballon">{text}</div>
+        <div className="btn_wrap">{button}</div> */}
     </div>
-  )
-}
+  );
+};
